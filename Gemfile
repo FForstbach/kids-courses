e76e90071a06bbdfea27064087e9f15c14534b57
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-05-23 13:01:56 +0200

    adds category column to courses

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'autoprefixer-rails'
 
 # my gems
 gem 'devise'
+gem 'faker'
 
 group :development, :test do
   gem 'binding_of_caller'
---
+++ db/migrate/20170523110128_add_category_to_courses.rb
@@ -0,0 +1,5 @@
+class AddCategoryToCourses < ActiveRecord::Migration[5.0]
+  def change
+    add_column :courses, :category, :string
+  end
+end
