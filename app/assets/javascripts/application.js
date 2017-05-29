c10b63ebfe7f6111956219a136f402dc68b34e0d
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-05-29 10:11:50 +0200

    adds algolia

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+//= require algolia/v3/algoliasearch.min
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,12 @@
 class Course < ApplicationRecord
   has_many :teachers
+
+  include AlgoliaSearch
+
+  algoliasearch do
+    attribute :title, :subtitle, :description
+    searchableAttributes ['title', 'subtitle', 'unordered(description)']
+  end
+
+
 end
